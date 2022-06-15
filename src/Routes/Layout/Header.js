3acc4942d2b251a9018a3624f063b38992f14c27
MizzleNA <mizzleaaa@gmail.com>
2022-06-16 00:03:18 +0900

    🚧 work login

--- src/Routes/Layout/Header.js
+++ src/Routes/Layout/Header.js
@@ -55,11 +55,6 @@ const Header = () => {
                                         </HeaderImages>
                                         <HeaderItem>
                                             <HeaderMenu
-                                                className={
-                                                    isNaN(location.pathname.substring(1))
-                                                        ? "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"
-                                                        : "border-indigo-500 text-gray-900"
-                                                }
                                                 onClick={() => navigate("/")}
                                             >
                                                 Template
